ffe34b25ddebf4b505feacf365bc5002cd26aa86
Rogier Mulders <rogier.mulders@xs4all.nl>
2024-06-12 15:01:05 +0200

    fix

--- src/BamServiceProvider.php
+++ src/BamServiceProvider.php
@@ -13,7 +13,7 @@ class BamServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'routes/web.php');
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
 
         // Register the command if we are using the application via the CLI
         if ($this->app->runningInConsole()) {
